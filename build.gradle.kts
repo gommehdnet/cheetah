118569c75b75be74f82f72669d322bec5c8cb81e
Jendrik Eggers <jendrikeggerskapp@web.de>
2024-02-12 02:22:59 +0100

    update upstream

--- build.gradle.kts
+++ build.gradle.kts
@@ -65,6 +65,13 @@ paperweight {
             serverPatchDir = layout.projectDirectory.dir("patches/server")
             serverOutputDir = layout.projectDirectory.dir("cheetah-server")
         }
+
+        patchTasks.register("generatedApi") {
+            isBareDirectory = true
+            upstreamDirPath = "paper-api-generator/generated"
+            patchDir = layout.projectDirectory.dir("patches/generated-api")
+            outputDir = layout.projectDirectory.dir("paper-api-generator/generated")
+        }
     }
 }
 
