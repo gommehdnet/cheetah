ee063c1cb7cf33a2de9d446a27df08f6d47f2d64
TehManu <tehmanu.developer@gmail.com>
2025-09-20 16:36:12 +0200

    Fix paper-plugin.yml package path and remove duplicated copying task

--- test-plugin/build.gradle.kts
+++ test-plugin/build.gradle.kts
@@ -1,13 +1,9 @@
-import kotlin.io.path.exists
-
 version = "1.0.0-SNAPSHOT"
 
 dependencies {
     compileOnly(project(":cheetah-api"))
     compileOnly(project(":cheetah-server"))
 }
-
-val pluginsDir: java.nio.file.Path = rootProject.layout.projectDirectory.dir("run/plugins").asFile.toPath()
 
 tasks.processResources {
     var apiVersion = rootProject.providers.gradleProperty("mcVersion").get()
@@ -21,19 +17,5 @@ tasks.processResources {
     inputs.properties(props)
     filesMatching("paper-plugin.yml") {
         expand(props)
-    }
-}
-
-if (pluginsDir.exists()) {
-    val copyArtifacts by tasks.registering(Copy::class) {
-        description = "Copy the test plugin jar to the test server plugins directory"
-        group = "build"
-
-        from(tasks.jar.flatMap { it.archiveFile })
-        into(pluginsDir)
-    }
-
-    tasks.assemble {
-        finalizedBy(copyArtifacts)
     }
 }
