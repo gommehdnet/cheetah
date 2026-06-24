baea927fba7c59815f78b624558b808cfe231c11
TehManu <tehmanu.developer@gmail.com>
2026-06-24 15:16:24 +0200

    feat: update to version 26.1.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,6 +12,18 @@ plugins {
 
 rootProject.name = "cheetah"
 
+gradle.lifecycle.beforeProject {
+    val mcVersion = providers.gradleProperty("mcVersion").get().trim()
+    val paperVersionChannel = providers.gradleProperty("channel").get().trim()
+    val paperBuildNumber = providers.environmentVariable("BUILD_NUMBER").orNull?.trim()?.toInt()
+    val versionString = if (paperBuildNumber == null) {
+        "$mcVersion.local-SNAPSHOT"
+    } else {
+        "$mcVersion.build.$paperBuildNumber-${paperVersionChannel.lowercase()}"
+    }
+    version = versionString
+}
+
 include("cheetah-api")
 include("cheetah-server")
 include("test-plugin")
